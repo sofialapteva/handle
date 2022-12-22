3044f815780c8063de68f3d811175285124ab72e
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-22 20:21:51 +0100

    establish connection

--- bot.mjs
+++ bot.mjs
@@ -1,8 +1,14 @@
 import TeleBot from "telebot";
 import * as dotenv from "dotenv";
+import * as cron from "node-cron";
 dotenv.config();
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => msg.reply.text(msg.text));
+bot.on("text", (msg) => {
+  msg.reply.text(JSON.stringify(msg));
+});
 
+// cron.schedule("0 9 * * MON", () => {
+//   bot.sendMessage(12345678, "scheduled message");
+// });
 export default bot;
