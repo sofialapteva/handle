cb317c38934ffdb426b45f6ae9e68ad8bc9d2663
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-22 20:36:59 +0100

    try node-cron

--- bot.mjs
+++ bot.mjs
@@ -4,11 +4,33 @@ import * as cron from "node-cron";
 dotenv.config();
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", (msg) => {
-  msg.reply.text(JSON.stringify(msg));
+// #  ┌──────────── minute
+// #  │ ┌────────── hour
+// #  │ │ ┌──────── day of month
+// #  │ │ │ ┌────── month
+// #  │ │ │ │ ┌──── day of week
+// #  │ │ │ │ │
+// #  │ │ │ │ │
+// #  * * * * *
+
+let tasks = [];
+
+bot.on(["/start"], (msg) => {
+  const wakeUp = cron.schedule("30 6 * * *", () => {
+    bot.sendMessage(msg.from.id, "Wake up!");
+  });
+  const exercise = cron.schedule("* * * * *", () => {
+    bot.sendMessage(msg.from.id, "Wake up!");
+  });
+  tasks.push(
+    { id: msg.from.id, task: wakeUp },
+    { id: msg.from.id, task: exercise }
+  );
 });
 
-// cron.schedule("0 9 * * MON", () => {
-//   bot.sendMessage(12345678, "scheduled message");
-// });
+bot.on(["/stop"], (msg) => {
+  tasks.filter((task) => task.id == msg.from.id).forEach((task) => task.stop());
+  tasks = tasks.filter((task) => task.id !== msg.from.id);
+});
+
 export default bot;
