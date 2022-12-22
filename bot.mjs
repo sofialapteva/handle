37b0cc43f77dd31f964df00bdf9122703d46155a
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-22 20:02:08 +0100

    add dotenv

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,8 @@
-import TeleBot from "telebot"
+import TeleBot from "telebot";
+import * as dotenv from "dotenv";
+dotenv.config();
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+bot.on("text", (msg) => msg.reply.text(msg.text));
 
-bot.on('text', msg => msg.reply.text(msg.text))
-
-export default bot
+export default bot;
