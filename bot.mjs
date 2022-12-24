173a255be05dbd1309d06ffacc5c9e31b5076d97
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-24 06:41:18 +0100

    set up keyboard

--- bot.mjs
+++ bot.mjs
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import * as cron from "node-cron";
 dotenv.config();
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
-const chatId = process.env.CHAT_ID || 0o0;
+const chatId = process.env.CHAT_ID;
 // #  ┌──────────── minute
 // #  │ ┌────────── hour
 // #  │ │ ┌──────── day of month
@@ -19,60 +19,28 @@ cron.schedule("* * * * * *", () => {
 });
 
 bot.on("/start", (msg) => {
-  let replyMarkup = bot.keyboard(
-    [
-      [
-        bot.button("contact", "Your contact"),
-        bot.button("location", "Your location"),
-      ],
-      ["/back", "/hide"],
-    ],
-    { resize: true }
-  );
+  let replyMarkup = bot.keyboard([["/cron", "/stop"]], { resize: true });
   bot.sendMessage(chatId, "Menu", { replyMarkup });
-
-  replyMarkup = bot.keyboard(
-    [
-      ["/buttons", "/inlineKeyboard"],
-      ["/start", "/hide"],
-    ],
-    { resize: true }
-  );
-
-  bot.sendMessage(chatId, "keyboard", { replyMarkup });
-  replyMarkup = bot.inlineKeyboard([
-    [
-      bot.inlineButton("callback", { callback: "this_is_data" }),
-      bot.inlineButton("inline", { inline: "some query" }),
-    ],
-    [bot.inlineButton("url", { url: "https://telegram.org" })],
-  ]);
-
-  return bot.sendMessage(msg.from.id, "Inline keyboard example.", {
-    replyMarkup,
-  });
-});
-
-bot.on("/schedule", (msg) => {
-  bot.sendMessage(chatId, "Wake up!");
-  const wakeUp = cron.schedule("30 6 * * *", () => {
-    bot.sendMessage(chatId, "Wake up!");
-  });
-  const exercise = cron.schedule("* * * * * *", () => {
-    bot.sendMessage(chatId, Date.now() + "");
-  });
-  tasks.push({ id: chatId, task: wakeUp }, { id: chatId, task: exercise });
 });
 
 // Inline button callback
 bot.on("callbackQuery", (msg) => {
-  console.log(msg.data);
-  bot.sendMessage(chatId, msg.data);
-});
+  if (msg.data === "cron") {
+    const wakeUp = cron.schedule("30 6 * * *", () => {
+      bot.sendMessage(chatId, "Wake up!");
+    });
+    const exercise = cron.schedule("* * * * * *", () => {
+      bot.sendMessage(chatId, Date.now() + "");
+    });
+    tasks.push({ id: chatId, task: wakeUp }, { id: chatId, task: exercise });
+    bot.sendMessage(chatId, "Starting cron work");
+  }
 
-bot.on("/stop", (msg) => {
-  tasks.filter((task) => task.id == chatId).forEach((task) => task.stop());
-  tasks = tasks.filter((task) => task.id !== chatId);
+  if (msg.data === "stop") {
+    tasks.filter((task) => task.id == chatId).forEach((task) => task.stop());
+    tasks = tasks.filter((task) => task.id !== chatId);
+    bot.sendMessage(chatId, "Stopping cron work");
+  }
 });
 
 export default bot;
